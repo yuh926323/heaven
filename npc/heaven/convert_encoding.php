eca73604dec867423a47d766f959ef0cb713ffbd
yuh926323 <yuh926323@gmail.com>
2017-11-30 02:20:12 +0800

    fix: from ... to enconding error

--- npc/heaven/convert_encoding.php
+++ npc/heaven/convert_encoding.php
@@ -7,7 +7,7 @@ fclose($file);
 
 echo "Convert filename: \033[0;37m{$full_filename}\033[0m ... \033[1;92mSuccessfully!\e[0m\n";
 
-$big5 = mb_convert_encoding($utf8, 'utf8', 'big5');
+$big5 = mb_convert_encoding($utf8, 'big5', 'utf8');
 $file = fopen($full_filename, 'w');
 fwrite($file, $big5);
 fclose($file);
